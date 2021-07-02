c94f29efc61e2cc87a575c25a62883fb1649132d
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-07-02 10:16:23 +0200

    Added simple navigation mechanism

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
 }
