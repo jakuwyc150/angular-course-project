f4e63e2740077fb5d7c9f4ae65661af797d85c54
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-07-03 11:41:36 +0200

    Added service class for recipes management

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,12 @@
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
