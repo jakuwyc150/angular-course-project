fb5137e73ff017007d0a770fd5bf4b20d45397b9
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-07-04 16:33:28 +0200

    Added routing module for project application

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,12 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
-
   constructor() {}
 
   ngOnInit(): void {
-  }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
   }
 }
