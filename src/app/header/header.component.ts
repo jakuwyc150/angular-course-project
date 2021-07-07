72b6b6e727a333c2b542c317c1b2445edfeb25fc
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-07-07 13:31:04 +0200

    Added HTTP interaction with backend (Firebase)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private dataStorageService: DataStorageService) {}
 
   ngOnInit(): void {
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
   }
 }
