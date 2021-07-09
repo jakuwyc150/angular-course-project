a82405d63f87eca8d97aa290d34229a82915d6c8
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-07-09 14:58:22 +0200

    Added lazy loading for shopping list module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(module => module.RecipesModule) }
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(module => module.RecipesModule) },
+  { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(module => module.ShoppingListModule) }
 ];
 
 @NgModule({
