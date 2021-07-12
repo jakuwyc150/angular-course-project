dec7a2a56379926524f0cde7e22acd8db79b202c
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-07-12 10:32:23 +0200

    Added Angular Universal SSR module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 // import { AuthService } from './auth/auth/auth.service';
 import { LoggingService } from './logging.service';
 import * as fromRoot from './store/app.reducer';
 import * as AuthActions from './auth/auth/store/auth.actions';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +15,17 @@ export class AppComponent implements OnInit {
   constructor(
     // private authService: AuthService,
     private store: Store<fromRoot.AppState>,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    @Inject(PLATFORM_ID) private platformID
   ) {}
 
   ngOnInit() {
     // this.authService.autoLogin();
-    this.store.dispatch(new AuthActions.AutoLogin());
+
+    if (isPlatformBrowser(this.platformID) === true) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
+
     this.loggingService.printLog("Hello from AppComponent ngOnInit");
   }
 }
